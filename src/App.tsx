44617af2c09919ec9daa4087a6edceb13961057a
amar-malakar <amarmalakarinfo@gmail.com>
2025-01-05 19:44:52 +0530

    initial setup vite + react + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="px-4 pt-16 text-center">
+      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md shadow-xl mt-4">CLICK ME!</button>
+    </main>
+  );
+}
+
+export default App;
